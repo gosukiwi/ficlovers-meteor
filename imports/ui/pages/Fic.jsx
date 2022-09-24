c7dc5f94de4cd6dc17b7485d4bbbe50871c7895f
Federico Ramirez <fedra.arg@gmail.com>
2022-09-24 17:29:41 -0300

    Fic show

--- imports/ui/pages/Fic.jsx
+++ imports/ui/pages/Fic.jsx
@@ -1,5 +1,5 @@
 import { Meteor } from "meteor/meteor";
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useTracker } from "meteor/react-meteor-data";
 import { Flex, Text, Box, Heading } from "@chakra-ui/react";
@@ -7,8 +7,9 @@ import { FicsCollection, ChaptersCollection } from "/imports/collections";
 
 export default function Fic() {
   const { id } = useParams();
+  const [currentChapter, setCurrentChapter] = useState(null);
   const fic = useTracker(() => {
-    const handler = Meteor.subscribe("fics.public");
+    const handler = Meteor.subscribe("fics.show", id);
 
     if (!handler.ready()) return null;
 
@@ -16,22 +17,23 @@ export default function Fic() {
   }, [id]);
 
   const chapters = useTracker(() => {
-    const handler = Meteor.subscribe("chapters.public");
+    const handler = Meteor.subscribe("chapters.show", id);
 
     if (!handler.ready()) return [];
 
-    return ChaptersCollection.find({ ficId: fic.id }).fetch();
-  }, [fic, id]);
+    const chapters = ChaptersCollection.find({ ficId: id }).fetch();
+    if (chapters.length > 0) setCurrentChapter(chapters[0]);
+    return chapters;
+  }, [id]);
 
-  console.log(chapters);
-
-  if (fic === null) return;
-  if (chapters.length === 0) return;
+  if (fic === null) return null;
+  if (chapters.length === 0) return null;
   // TODO: Loading
 
   return (
-    <Flex>
-      <Heading>{fic.title}</Heading>
+    <Flex direction="column">
+      <Heading mb={3}>{fic.title}</Heading>
+      <Text>{currentChapter.body}</Text>
     </Flex>
   );
 }
--- server/publications.js
+++ server/publications.js
@@ -1,12 +1,24 @@
 import { Meteor } from "meteor/meteor";
+import { check } from "meteor/check";
 import { FicsCollection, ChaptersCollection } from "/imports/collections";
 
 Meteor.publish("fics.user", function () {
   return FicsCollection.find({ userId: this.userId });
 });
 
-Meteor.publish("fics.public", function () {
-  return FicsCollection.find({ status: "published" });
+Meteor.publish("fics.show", (id) => {
+  check(id, String);
+
+  return FicsCollection.find({ _id: id, status: "published" });
+});
+
+Meteor.publish("chapters.show", function (ficId) {
+  check(ficId, String);
+
+  const fic = FicsCollection.findOne({ _id: ficId, status: "published" });
+  if (!fic) return this.ready(); // empty if fic is not published
+
+  return ChaptersCollection.find({ ficId });
 });
 
 Meteor.publish("chapters.user", function () {
